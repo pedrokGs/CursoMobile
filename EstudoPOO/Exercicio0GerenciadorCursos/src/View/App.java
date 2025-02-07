559b859717ca5e981baa356c55b3cf17e314334d
PedroHGon <pedibr5@gmail.com>
2025-02-07 16:40:51 -0300

    07.02.2025

--- EstudoPOO/Exercicio0GerenciadorCursos/src/View/App.java
+++ EstudoPOO/Exercicio0GerenciadorCursos/src/View/App.java
@@ -19,22 +19,23 @@ public class App {
             System.out.println("Escolha a opção desejada");
             System.out.println("1. Adicionar aluno");
             System.out.println("2. Informações do curso");
-            System.out.println("3. Sair");
+            System.out.println("3. Exibir status dos alunos");
+            System.out.println("4. Sair");
             System.out.println("=x=x=x=x=x=x=x=x=x=x=x=x=x=");
             Scanner sc = new Scanner(System.in);
 
-                operacao = sc.nextInt();
+            operacao = sc.nextInt();
 
             switch (operacao) {
                 case 1:
                     System.out.println("Insira o nome do aluno: ");
-                    String nomeAluno = sc.nextLine();
+                    String nomeAluno = sc.next();
 
                     System.out.println("Digite o cpf do aluno: ");
-                    String cpfAluno = sc.nextLine();
+                    String cpfAluno = sc.next();
 
                     System.out.println("Digite a matrícula do aluno: ");
-                    String matriculaAluno = sc.nextLine();
+                    String matriculaAluno = sc.next();
 
                     System.out.println("Digite a nota do aluno: ");
                     double notaAluno = sc.nextDouble();
@@ -44,17 +45,21 @@ public class App {
 
                     System.out.println("Aluno adicionado com sucesso!");
                     break;
-                
+
                 case 2:
                     curso1.exibirInformacoesCurso();
                     break;
 
                 case 3:
+                    curso1.exibirStatusAluno();
+                    break;
+
+                case 4:
 
                     System.out.println("Encerrando o sistema");
                     continuar = false;
                     break;
-                    
+
                 default:
                     System.out.println("Opção inválida!");
                     break;
