87a5c9c80ac4cf4b228dc6c20984b3cc136af85a
PedroHGon <pedibr5@gmail.com>
2025-04-25 16:56:23 -0300

    25.04.2025

--- persistencia_de_dados/shared_preferences/sa_perfil/sa_perfil/lib/views/perfil.dart
+++ persistencia_de_dados/shared_preferences/sa_perfil/sa_perfil/lib/views/perfil.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 
 class PerfilPage extends StatefulWidget {
@@ -23,7 +24,7 @@ class _PerfilPageState extends State<PerfilPage> {
       body: Padding(
         padding: EdgeInsets.all(5),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
@@ -74,6 +75,33 @@ class _PerfilPageState extends State<PerfilPage> {
               child: Text(
                 "Idade:  ${idade == 0 ? "Desconhecida" : idade}",
                 style: TextStyle(color: Colors.black87, fontSize: 48),
+              ),
+            ),
+                        SizedBox(height: 24),
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.25,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white70,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black87,
+                    blurRadius: 10,
+                    spreadRadius: 0.5,
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text("Cor Preferida:"),
+                  SizedBox(height: 24,),
+                  ElevatedButton(onPressed: () {
+                    
+                  }, child: Text("Escolher uma cor"))
+                ],
+              )
               ),
             ),
           ],
